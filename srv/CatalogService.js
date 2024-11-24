6233d3ca3e697c0d6304313874bee39808029a58
CarlosGuzmanCG <66038506+CarlosGuzmanCG@users.noreply.github.com>
2024-11-23 22:59:35 -0600

    métdo post con validación de registros

--- srv/CatalogService.js
+++ srv/CatalogService.js
@@ -1,4 +1,5 @@
 const cds =  require('@sap/cds');
+const { message } = require('@sap/cds/lib/log/cds-error');
 const MongoClient = require('mongodb').MongoClient;
 const dotenv = require('dotenv');
 dotenv.config();
@@ -8,6 +9,20 @@ const sDbName = process.env.DATABASE_NAME;
 const client = new MongoClient(sMongoUrl, { useUnifiedTopology: true });
 
 async function _CrearSuperheroe(req) {
+
+    if(req.data.franquicia != "Marvel" && req.data.franquicia != "DC") {
+        req.error({code:400, message:"La franquicia debe ser Marvel o DC"});
+    }
+
+    await client.connect();
+    let database = await client.db(sDbName);
+    let superheroe = await database.collection('superHeroes');
+    let result = await superheroe.insertOne(req.data);
+
+    if (result.insertedId) {
+        req.data.id = result.insertedId;
+    }
+
     return req.data;
 }
 
